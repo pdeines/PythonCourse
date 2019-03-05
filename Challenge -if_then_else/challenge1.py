266184d19f336bff6f1d78d7a43dd29244fe0c8c
Paul Deines <paul.deines@gmail.com>
2019-03-04 20:49:15 -0800

    helloworld + if_elif_else conditions

---
+++ Challenge -if_then_else/challenge1.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+if (age > 17) and (age < 31):
+    print("Welcome to the party")
+else:
+    print("Sorry but you are not the right age.")
--- HelloWorld/helloworld.py
+++ HelloWorld/helloworld.py
@@ -1,4 +1,11 @@
 print("Hello World!")
 greeting = "Hello"
-name = input("Please enter your name ")
-print(greeting + " " + name)
+
+days = "Mon, Tue, Wed, Thu, Fri, Sat, Sun"
+print(days[::5])
+
+data = "1:A, 2:B, 3:C, 4:D, 5:E, 6:F, 7:G, 8:H"
+print(data[::5])
+print(data[1:5])
+print(data[0:-1:5])
+print(data[:-1:5])
